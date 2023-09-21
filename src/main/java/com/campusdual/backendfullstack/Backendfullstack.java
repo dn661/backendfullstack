e257a6b7c23c04849c3dccfd19ea4954d7e2eaf1
Belial <xxdani661xx@gmail.com>
2023-09-21 13:50:22 +0200

    Initial commit

---
+++ src/main/java/com/campusdual/backendfullstack/Backendfullstack.java
@@ -0,0 +1,13 @@
+package com.campusdual.backendfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendfullstack {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendfullstack.class, args);
+	}
+
+}
