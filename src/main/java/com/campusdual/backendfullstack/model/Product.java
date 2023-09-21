345059b6d89085ce436a0de7cf3ccce645925520
Belial <xxdani661xx@gmail.com>
2023-09-21 14:13:22 +0200

    creadas las clases para products y contact

--- src/main/java/com/campusdual/backendfullstack/model/Product.java
+++ src/main/java/com/campusdual/backendfullstack/model/Product.java
@@ -7,6 +7,7 @@ import java.util.Date;
 @Entity
 @Table(name="PRODUCTS")
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int ID;
@@ -25,4 +26,52 @@ public class Product {
 
     @Column
     private Date date_added;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getDate_added() {
+        return date_added;
+    }
+
+    public void setDate_added(Date date_added) {
+        this.date_added = date_added;
+    }
+
 }
